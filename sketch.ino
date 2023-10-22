b6c5a8359952db1b3005b9a6c51c032bc97a2298
Kosamela <57198990+Kosamela@users.noreply.github.com>
2023-10-22 21:16:20 +0200

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,67 @@
+/*
+Szkic Arduino Diody LED
+Autor: Kamil Kosmela, 22-10-2023
+*/
+
+#define LED_WR 5
+#define LED_WY 6
+#define LED_WG 7
+#define LED_ER 10
+#define LED_EY 11
+#define LED_EG 12
+#define BUTTON_W 4
+#define BUTTON_E 3
+int buttonState_W = 0;
+int buttonState_E = 0;
+
+void setup()
+{
+pinMode(LED_WR, OUTPUT); // pin wyjściowy dla diody LED west red
+pinMode(LED_WY, OUTPUT); // pin wyjściowy dla diody LED west yellow
+pinMode(LED_WG, OUTPUT); // pin wyjściowy dla diody LED west green
+pinMode(LED_ER, OUTPUT); // pin wyjściowy dla diody LED east red
+pinMode(LED_EY, OUTPUT); // pin wyjściowy dla diody LED east yellow
+pinMode(LED_EG, OUTPUT); // pin wyjściowy dla diody LED east green
+pinMode(BUTTON_W, INPUT); // pin wejściowy dla przycisku west
+pinMode(BUTTON_E, INPUT); // pin wejściowy dla przycisku east
+digitalWrite(LED_WR, HIGH);
+digitalWrite(LED_EG, HIGH);
+}
+
+void loop()
+{
+buttonState_W = digitalRead(BUTTON_W);
+buttonState_E = digitalRead(BUTTON_E);
+
+
+if ( buttonState_W == HIGH && digitalRead(LED_EG) == HIGH)
+{
+digitalWrite(LED_EG, LOW);
+digitalWrite(LED_EY, HIGH);
+digitalWrite(LED_ER, HIGH);
+delay(2000);
+digitalWrite(LED_EY, LOW);
+delay(3000); // czekamy, az pojazdy zdaza przejechac most
+// otwieramy ruch dla west
+digitalWrite(LED_WR, LOW); 
+digitalWrite(LED_WY, HIGH); 
+delay(2000);
+digitalWrite(LED_WY, LOW); 
+digitalWrite(LED_WG, HIGH);
+}
+if ( buttonState_E == HIGH && digitalRead(LED_WG) == HIGH )
+{
+digitalWrite(LED_WG, LOW);
+digitalWrite(LED_WY, HIGH);
+digitalWrite(LED_WR, HIGH);
+delay(2000);
+digitalWrite(LED_WY, LOW);
+delay(3000); // czekamy, az pojazdy zdaza przejechac most
+// otwieramy ruch dla east
+digitalWrite(LED_ER, LOW); 
+digitalWrite(LED_EY, HIGH); 
+delay(2000);
+digitalWrite(LED_EY, LOW); 
+digitalWrite(LED_EG, HIGH);
+}
+}
